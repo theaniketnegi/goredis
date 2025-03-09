b51d728c204e43593196e2b62822eedda10fb2de
Aniket Negi <aniketng08@gmail.com>
2025-03-09 19:05:32 +0530

    SET with Expiry

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 	"io"
 	"log"
 	"net"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/theaniketnegi/goredis/parser"
 	"github.com/theaniketnegi/goredis/store"
@@ -59,14 +62,137 @@ func connectionHandler(conn net.Conn, store *store.InMemoryStore) {
 				conn.Write([]byte("_\r\n"))
 				continue
 			}
-			conn.Write(fmt.Appendf(nil, "$%d\r\n%s\r\n", len(val), val))
+			conn.Write(fmt.Appendf(nil, "$%d\r\n%s\r\n", len(val.Value), val.Value))
 		case "SET":
-			if len(args) != 2 {
+			if len(args) < 2 {
 				conn.Write([]byte("-ERR wrong number of arguments for 'set' command\r\n"))
 				continue
 			}
-			store.Set(args[0], args[1])
-			conn.Write([]byte("+OK\r\n"))
+
+			var expiry *time.Time = nil
+
+			expiryCmd := ""
+			var nx bool = false
+			var xx bool = false
+			var ttl bool = false
+			var get bool = false
+
+			var errFlag = 0
+
+			for i := 2; i < len(args); {
+				cmd := strings.ToUpper(args[i])
+				switch cmd {
+				case "EX":
+					if (expiryCmd != "" && expiryCmd != cmd) || ttl {
+						conn.Write([]byte("-ERR Syntax error\r\n"))
+						errFlag = 1
+						goto errHandler
+					}
+
+					if i+1 >= len(args) {
+						conn.Write([]byte("-ERR Syntax error\r\n"))
+						errFlag = 1
+						goto errHandler
+					}
+
+					seconds, err := strconv.Atoi(args[i+1])
+
+					if err != nil || seconds <= 0 {
+						conn.Write([]byte("-ERR invalid expire time in 'set' command\r\n"))
+						errFlag = 1
+						goto errHandler
+					}
+
+					expire := time.Now().Add(time.Duration(seconds) * time.Second)
+					expiry = &expire
+
+					expiryCmd = cmd
+					i += 2
+				case "PX":
+					if (expiryCmd != "" && expiryCmd != cmd) || ttl {
+						conn.Write([]byte("-ERR Syntax error\r\n"))
+						errFlag = 1
+						goto errHandler
+					}
+
+					if i+1 >= len(args) {
+						conn.Write([]byte("-ERR Syntax error\r\n"))
+						errFlag = 1
+						goto errHandler
+					}
+
+					milliseconds, err := strconv.Atoi(args[i+1])
+
+					if err != nil || milliseconds <= 0 {
+						conn.Write([]byte("-ERR invalid expire time in 'set' command\r\n"))
+						errFlag = 1
+						goto errHandler
+					}
+
+					expire := time.Now().Add(time.Duration(milliseconds) * time.Millisecond)
+					expiry = &expire
+					expiryCmd = cmd
+					i += 2
+				case "KEEPTTL":
+					if expiryCmd != "" && expiryCmd != cmd {
+						conn.Write([]byte("-ERR Syntax error\r\n"))
+						errFlag = 1
+						goto errHandler
+					}
+					expiryCmd = cmd
+					ttl = true
+					i++
+				case "NX":
+					if xx {
+						conn.Write([]byte("-ERR Syntax error\r\n"))
+						errFlag = 1
+						goto errHandler
+					}
+					nx = true
+					i++
+				case "XX":
+					if nx {
+						conn.Write([]byte("-ERR Syntax error\r\n"))
+						errFlag = 1
+						goto errHandler
+					}
+					xx = true
+					i++
+				case "GET":
+					get = true
+					i++
+				default:
+					conn.Write([]byte("-ERR Syntax error\r\n"))
+					errFlag = 1
+					goto errHandler
+				}
+			}
+
+		errHandler:
+			if errFlag == 1 {
+				continue
+			}
+
+			fmt.Println(expiry)
+			conn.Write([]byte(store.Set(args[0], args[1], expiry, nx, xx, ttl, get)))
+		case "TTL":
+			if len(args) != 1 {
+				conn.Write([]byte("-ERR wrong number of arguments for 'ttl' command\r\n"))
+				continue
+			}
+
+			storeVal, ok := store.Get(args[0])
+			if !ok {
+				conn.Write([]byte(":-2\r\n"))
+				continue
+			}
+
+			if storeVal.Expiry == nil {
+				conn.Write([]byte(":-1\r\n"))
+				continue
+			}
+			fmt.Println(time.Until(*storeVal.Expiry).Seconds())
+			conn.Write(fmt.Appendf(nil, ":%d\r\n", int64(time.Until(*storeVal.Expiry).Seconds())))
 		default:
 			conn.Write([]byte("+PONG\r\n"))
 		}
--- store/store.go
+++ store/store.go
@@ -1,19 +1,27 @@
 package store
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
+type StoreValue struct {
+	Value  string
+	Expiry *time.Time
+}
 type InMemoryStore struct {
-	storage map[string]string
+	storage map[string]StoreValue
 	mu      sync.RWMutex
 }
 
 func NewInMemoryStore() *InMemoryStore {
 	return &InMemoryStore{
-		storage: make(map[string]string),
+		storage: make(map[string]StoreValue),
 	}
 }
 
-func (s *InMemoryStore) Get(key string) (string, bool) {
+func (s *InMemoryStore) Get(key string) (StoreValue, bool) {
 	s.mu.RLock()
 	defer s.mu.RUnlock()
 
@@ -22,9 +30,32 @@ func (s *InMemoryStore) Get(key string) (string, bool) {
 	return value, ok
 }
 
-func (s *InMemoryStore) Set(key string, value string) {
+func (s *InMemoryStore) Set(key string, value string, expiry *time.Time, nx bool, xx bool, ttl bool, get bool) string {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
-	s.storage[key] = value
+	oldVal, ok := s.storage[key]
+
+	if nx && ok {
+		return "_\r\n"
+	}
+
+	if xx && !ok {
+		return "_\r\n"
+	}
+
+	if ttl {
+		expiry = oldVal.Expiry
+	}
+
+	s.storage[key] = StoreValue{Value: value, Expiry: expiry}
+	if get {
+		if ok {
+			return fmt.Sprintf("$%d\r\n%s\r\n", len(oldVal.Value), oldVal.Value)
+		} else {
+			return "_\r\n"
+		}
+	}
+
+	return "+OK\r\n"
 }
